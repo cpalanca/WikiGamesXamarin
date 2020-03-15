9ceb3391e9bb26f59fd78046b75697c31e52b5e4
Carlos P <izv.palancagimenezcarlos@gmail.com>
2020-03-15 19:02:48 +0100

    update references

--- WikiGames/Client/StatusResponse.cs
+++ WikiGames/Client/StatusResponse.cs
@@ -9,7 +9,12 @@ namespace WikiGames.Client
         // https://www.youtube.com/watch?v=xtjYfhtHmrc
         // 1:23:35
         // https://app.quicktype.io/#l=cs&r=json2csharp
-
+        /*
+        https://docs.microsoft.com/es-es/xamarin/xamarin-forms/app-fundamentals/navigation/master-detail-page
+        https://stackoverflow.com/questions/37179096/xamarin-forms-google-navigation-drawer
+        Formando Código Curso Xamarin https://www.youtube.com/watch?v=SPTHzbj0uwY
+        CódigoFacilito Curso Xamarin https://www.youtube.com/watch?v=07bukMZh05U
+         */
 
     }
 }
